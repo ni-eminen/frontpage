922d20ef3048a5e0de61866459484087baf0da4d
ni-eminen <matiasnie2@gmail.com>
2021-11-10 21:29:48 +0200

    added headers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+
+const Button = styled.button`
+  margin: 1em auto;
+  border: 0px solid black;
+  background-color: RGB(232, 89, 12, .75);
+  color: black;
+  padding: 14px 28px;
+  font-size: 16px;
+  width: 100%;
+  color: white;
+
+  &:hover {
+    background-color: RGB(232, 89, 12, 1);
+    cursor: pointer;
+  }
+  `
+  const openWebsite = (url) => {
+    window.open(url, "_blank")
+  }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <h1>Projects</h1>
+      <Button className="linkButton" onClick={() => openWebsite('./speed-test')}>
+        1. Typing speed test
+      </Button>
+      <Button className="linkButton" onClick={() => openWebsite('./NewQuiz')}>
+        2. Commission by JYY
+      </Button>
+      <h1>Git</h1>
+      <Button className="linkButton" onClick={() => openWebsite('./NewQuiz')}>
+        3. GitHub
+      </Button>
+      <Button className="linkButton" onClick={() => openWebsite('./NewQuiz')}>
+        4. GitLab
+      </Button>
     </div>
-  );
+  )
 }
 
 export default App;
