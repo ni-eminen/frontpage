5e3df567397eacb28c6ecb79431a722d9470686f
ni-eminen <matiasnie2@gmail.com>
2022-01-05 03:01:56 +0200

    misc

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import styled from 'styled-components'
 import './App.css';
 
 const Button = styled.button`
-  margin: 1em auto;
+  max-width: 100%;
   border: 0px solid black;
   background-color: RGB(232, 89, 12, .75);
   color: black;
   padding: 14px 28px;
   font-size: 16px;
-  width: 100%;
   color: white;
 
   &:hover {
@@ -25,18 +24,26 @@ function App() {
   return (
     <div id="wrapper">
       <h1>Projects</h1>
-      <Button className="linkButton" onClick={() => openWebsite('./speed-test')}>
-        1. Typing speed test
-      </Button>
+      <div className='projectWrapper'>
+        <Button className="linkButton" onClick={() => openWebsite('./speed-test')}>
+          1. Typing speed test
+        </Button>
+        <Button className="gitButton" onClick={() => openWebsite('https://github.com/ni-eminen/TypingSpeedTest')}>
+          Git
+        </Button>
+      </div>
       <Button className="linkButton" onClick={() => openWebsite('./NewQuiz')}>
         2. Commission by JYY
       </Button>
+      <Button className="gitButton" onClick={() => openWebsite('https://github.com/ni-eminen/jyy-peli')}>
+          Git
+        </Button>
       <h1>Git</h1>
-      <Button className="linkButton" onClick={() => openWebsite('https://github.com/ni-eminen')}>
+      <Button className='linkButton' onClick={() => openWebsite('https://github.com/ni-eminen')}>
         3. GitHub
       </Button>
-      <Button className="linkButton" onClick={() => openWebsite('https://gitlab.jyu.fi/matoskni')}>
-        4. GitLab
+      <Button className='linkButton' onClick={() => openWebsite('https://gitlab.jyu.fi/matoskni')}>
+        4. GitLab - Inactive
       </Button>
     </div>
   )
